f6c1350ebd37519f0b021311be92180e83451ce3
Kyrylo Zakharov <kirillzakharov13@gmail.com>
2025-11-06 16:15:35 +0200

    Additional layer against clickjacking

--- docs/scripts/prevent-clickjacking.js
+++ docs/scripts/prevent-clickjacking.js
@@ -1,9 +1,12 @@
 function preventClickjacking() {
-    if (window.top !== window.self) {
-        window.top.location = window.self.location;
-    }
+  if (window.self === window.top) {
+    const antiClickjack = document.getElementById('antiClickjack')
+    antiClickjack.parentNode.removeChild(antiClickjack)
+  } else {
+    window.top.location = window.self.location
+  }
 }
 
-window.addEventListener('DOMContentLoaded', preventClickjacking);
-window.addEventListener('load', preventClickjacking);
-window.addEventListener('resize', preventClickjacking);
+window.addEventListener('DOMContentLoaded', preventClickjacking)
+window.addEventListener('load', preventClickjacking)
+window.addEventListener('resize', preventClickjacking)
